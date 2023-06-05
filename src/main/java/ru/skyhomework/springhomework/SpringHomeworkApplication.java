4ce9612ded533a6780fe1dc454f9072da8c25023
rom064 <rom064@mail.ru>
2023-06-05 23:13:24 +0500

    ДЗ Spring

---
+++ src/main/java/ru/skyhomework/springhomework/SpringHomeworkApplication.java
@@ -0,0 +1,13 @@
+package ru.skyhomework.springhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skyhomework/springhomework/SpringHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skyhomework.springhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
